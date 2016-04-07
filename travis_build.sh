46d2bfbc4898183a054ce43ab6308e628bf1f5b4
Ethan Rublee <ethan.rublee@gmail.com>
2016-04-07 12:32:21 -0700

    Don't build container as part of travis.

--- travis_build.sh
+++ travis_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
-export OPENCVAI_DEV_ENV_TAG=:travis
-docker pull opencvai/dev_env16.04:latest
-docker build -t opencvai/dev_env16.04$OPENCVAI_DEV_ENV_TAG -f dev_env16.04/Dockerfile ./
+# Don't build the container... it takes too long.
+# export OPENCVAI_DEV_ENV_TAG=:travis
+# docker build -t opencvai/dev_env16.04$OPENCVAI_DEV_ENV_TAG -f dev_env16.04/Dockerfile ./
 . path.bash.inc
 mkdir -p build && cd build
 cv_cmake_init ../sample
