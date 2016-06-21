71ab1a6313587e6e5a5f5fd819a7b81a712fbbc5
Ethan Rublee <ethan.rublee@gmail.com>
2016-06-20 21:54:17 -0700

    Yup.

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 #docker build -t opencvai/dev_env16.04 -f dev_env16.04/Dockerfile ./
 #docker build -t opencvai/dev_env14.04 -f dev_env14.04/Dockerfile ./
 docker build -t opencvai/mini_dev_env14.04 -f mini_dev_env14.04/Dockerfile ./
-docker build -t opencvai/vision_dev_env14.04 -f vision_dev_env14.04/Dockerfile ./
+#docker build -t opencvai/vision_dev_env14.04 -f vision_dev_env14.04/Dockerfile ./
